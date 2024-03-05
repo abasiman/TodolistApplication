676eeda32bf83bab1ea94039ea0ca6a74cbc0b63
abasiman <abasimancode@gmail.com>
2024-03-06 01:47:47 +0700

    Update CORS

--- src/main.py
+++ src/main.py
@@ -17,7 +17,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
     allow_origins = ['https://todolist-application-wheat.vercel.app'],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
