c272bf0fd46362a31add761f7135b72dbe762256
abasiman <abasimancode@gmail.com>
2024-03-06 01:44:46 +0700

    Update todo list functionality

--- src/main.py
+++ src/main.py
@@ -13,13 +13,17 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173"], 
+    allow_origins = ['https://todolist-application-wheat.vercel.app'],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
+
 
 # create pydantic models
 
