c7bf91b19e58f546aee0ecfa3cbb89cafb88c480
abasiman <abasimancode@gmail.com>
2024-03-07 16:55:16 +0700

    environmental variables

--- src/database.py
+++ src/database.py
@@ -1,12 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
+# Read environment variables
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST')
+DB_NAME = os.environ.get('DB_NAME')
 
-URL_DATABASE = 'mysql+mysqlconnector://abasIman:Hassan 7878@localhost/TodoApp'
+# Construct the database URL
+DATABASE_URL = f'mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
 
-engine = create_engine(URL_DATABASE)
+# Create the database engine using the constructed URL
+engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush = False, bind = engine)
+# SessionLocal will be used for DB sessions
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base class for ORM models
 Base = declarative_base()
--- src/main.py
+++ src/main.py
@@ -7,22 +7,16 @@ from models import TodoList, CompletedList
 from fastapi.middleware.cors import CORSMiddleware
 
 
-
-
-
 app = FastAPI()
 
 
-from fastapi.middleware.cors import CORSMiddleware
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ['https://todolist-application-wheat.vercel.app'],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
-
 
 # create pydantic models
 
